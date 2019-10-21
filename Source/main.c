c2e4cd169abbb65f235679e322a9e5e7ff572474
MiguelMontoya <mickey056_lefay@hotmail.com>
2019-10-20 22:23:10 -0500

    Modifique el main.c

--- Source/main.c
+++ Source/main.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 #include<string.h>
-struct cita
-{
-  char nombrePaciente[20][20];	//arreglo para guardar las 16 citas y los 4 tiempos libres
-  int n;			//cantidad de citas restantes
-  int tamano;			//tamano total de la cola
-};
+#include"cita.h"
 int
 main (void)
 {
-  printf ("\nAgregue esto para tener una base para meter las funciones");
-  printf ("\nEliminar despues");
+struct cita Lunes;
+struct cita Martes;
+struct cita Miercoles;
+struct cita Jueves;
+struct cita Viernes;
+struct cita Sabado;
+int opcion=0;
+printf("	Bienvenido al sistema de registro de citas del consultorio ''Similares''\n");
+printf("Seleccione una opcion por favor\n");
+printf("1-. Agendar una cita	2-.Consultar la cita\n3-. Cancelar la cita	4-. Modificar su cita\n5-.Mostrar todas las citas para el dia	6-.Salir");
+scanf("%d",&opcion);
   return 0;
 }
