52c266bf50c7e865815bbaf4ea37ffecdf9918b1
MiguelMontoya <mickey056_lefay@hotmail.com>
2019-10-20 22:29:55 -0500

    Indente el main.c

--- Source/main.c
+++ Source/main.c
@@ -2,16 +2,18 @@
 int
 main (void)
 {
-struct cita Lunes;
-struct cita Martes;
-struct cita Miercoles;
-struct cita Jueves;
-struct cita Viernes;
-struct cita Sabado;
-int opcion=0;
-printf("	Bienvenido al sistema de registro de citas del consultorio ''Similares''\n");
-printf("Seleccione una opcion por favor\n");
-printf("1-. Agendar una cita	2-.Consultar la cita\n3-. Cancelar la cita	4-. Modificar su cita\n5-.Mostrar todas las citas para el dia	6-.Salir");
-scanf("%d",&opcion);
+  struct cita Lunes;
+  struct cita Martes;
+  struct cita Miercoles;
+  struct cita Jueves;
+  struct cita Viernes;
+  struct cita Sabado;
+  int opcion = 0;
+  printf
+    ("	Bienvenido al sistema de registro de citas del consultorio ''Similares''\n");
+  printf ("Seleccione una opcion por favor\n");
+  printf
+    ("1-. Agendar una cita	2-.Consultar la cita\n3-. Cancelar la cita	4-. Modificar su cita\n5-.Mostrar todas las citas para el dia	6-.Salir");
+  scanf ("%d", &opcion);
   return 0;
 }
